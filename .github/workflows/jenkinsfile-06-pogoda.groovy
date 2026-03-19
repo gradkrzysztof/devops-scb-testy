276dfbb7d0817600062f4407f8ee06fb68c729df
Krzysztof Grad <gradk86@gmail.com>
2026-03-19 10:11:40 +0100

    1st jenkinsfile v1

---
+++ .github/workflows/jenkinsfile-06-pogoda.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    agent {
+        label 'dev'
+    }
+    parameters {
+        choice(name: 'whichScript', choices: ['bash', 'python'], description: 'Wybierz skrypt ktory uruchomimy')
+        string(name: 'city', defaultValue: 'Warsaw', description: 'Wprowadz nazwe miasta do sprawdzenia pogody')
+        string(name: 'myApiKey', defaultValue: '7cff9972d5c98a9a47ddf6a59cb34d8e', description: 'Wprowadz klucz API do pogody')
+    }
+}
+
+stages {
+    stage('Wybrales bash') {
+        when {
+            expression { params.whichScript == 'bash' }
+        }
+        steps {
+            sh "bash ./cwiczenia/pogoda/start.sh '${whichScript}' '${myApiKey}'"
+        }
+    }
+    stage('Wybrales python') {
+        when {
+            expression { params.whichScript == 'python' }
+        }
+        steps {
+            sh "python3 ./cwiczenia/pogoda_python/start.py '${whichScript}' '${myApiKey}'"
+        }
+    }
+}
